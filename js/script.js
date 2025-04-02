5a129304151aad59ed4694bad05851d15609ed09
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-04-02 12:54:18 -0400

    flowchart and storyboard

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaac Ip All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaac Ip
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+
+/**
+* Do basic math.
+*/
+function doMathClicked() {
+    // input
+    const radius = parseFloat(document.getElementById("radius").value)
+    
+    
+    // process
+    const volumeOfSphere = (4 / 3) * Math.PI * radius**3
+
+
+    // output
+    document.getElementById("volume").innerHTML =
+      "<p>The volume is: " + volumeOfSphere.toFixed(2) + " mm³. </p>"
+   }
+  
