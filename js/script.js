fec0e869388564415c665b69f8ad52487c29ed89
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-05-17 17:03:03 -0400

    test 1

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@
 // Created on: Mar 2025
 // This file contains the JS functions for index.html
 
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICD2O-Unit-3-03-Isaac-Ip/sw.js", {
+    scope: "/ICD2O-Unit-3-03-Isaac-Ip/",
+  });
+}
 
 /**
 * Do basic math.
@@ -12,12 +20,17 @@ function doMathClicked() {
     // input
     const radius = parseFloat(document.getElementById("radius").value)
 
-
     // process
     const volumeOfSphere = (4 / 3) * Math.PI * radius ** 3
-
+    localStorage.setItem("volume", "volumeOfSphere");
 
     // output
     document.getElementById("volume").innerHTML =
         "<p>The volume is: " + volumeOfSphere.toFixed(2) + " mm³. </p>"
+}
+
+function updateVolume(){
+    localStorage.getItem("volume")
+        document.getElementById("volume").innerHTML =
+        "<p>The volume is: " + volume + " mm³. </p>"
 }
